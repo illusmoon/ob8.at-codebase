c1d4ecbe42d1de3aee4ce121e1e1b848f490a037
Christian Rhomberg <crhomber@l412.office.rhomberg.org>
2011-04-19 09:07:06 +0200

    updated nodewords-6.x-1.12-rc1.tar.gz

--- drupal6_modules/nodewords/nodewords.api.php
+++ drupal6_modules/nodewords/nodewords.api.php
@@ -1,6 +1,5 @@
 <?php
 
-// $Id: nodewords.api.php,v 1.1.2.8 2009/12/28 21:28:17 kiam Exp $
 
 /**
  * @file.
@@ -8,94 +7,145 @@
  */
 
 /**
- * The hook is used from nodewords.module to know which API is supported by the
- * the module.
+ * Reports the API is supported by the modules implementing meta tags.
  *
  * @return
  * An array containing the following indexes:
  *
+ *   - path - the path where the files for the integration with Nodewords are
+ *     placed.
  *   - version - the API version used by the module; basing on this value
  *     Nodewords will take the necessary steps to assure to keep the module
- *     compatible with Nodewords (Nodewords actually uses the meta tags
- *     implemented by a module that supports version 1.1, or 1.3 of
- *     Nodewords API).
- *
- */
+ *     compatible with Nodewords, The minimum API currently supported by the
+ *     module is contained in the constant NODEWORDS_MINIMUM_API_VERSION, and
+ *     the current API version is contained in the constant
+ *     NODEWORDS_API_VERSION.
+*/
 function hook_nodewords_api() {
-  return array('version' => '1.1');
+  return array('version' => '1.14', 'path' => '');
 }
 
 /**
- * This hook declares the meta tags implemented by the module.
+ * Alters the default meta tags values.
+ *
+ * @param &$values
+ *  The array of default meta tags values.
+ * @param $parameters
+ *  An array of parameters. The currently defined are:
+ *   * type - the type of object for the page to which the meta
+ *     tags are associated.
+ *   * id - The ID for the object associated with the page.
+ *   * phase - when it's 'pre_load', the hook is called before the meta tags
+ *     loaded from the database.
+ */
+function hook_nodewords_default_values_alter(&$values, $parameters) {
+  if (!empty($values['abstract']) && $parameters['type'] == NODEWORDS_TYPE_USER) {
+    $values['abstract'] = t('User profile');
+  }
+}
+
+/**
+ * Notifies modules when meta tags are deleted.
+ *
+ * @param $options.
+ *   An array of options that allows to identify the meta tags being deleted.
+ */
+function hook_nodewords_delete_tags($options) {
+  if ($options['type'] == NODEWORDS_TYPE_PAGE) {
+    db_query("DELETE FROM {nodewords_custom} WHERE pid = '%s'", $options['id']);
+  }
+}
+
+/**
+ * Changes the permission a user has on the meta tags being edited.
+ *
+ * @param &$permission
+ *   TRUE, if the user can edit the current meta tag.
+ * @param $object
+ *   An array describing the object to which the meta tag are associated.
+ * @param $tag_name
+ *   The name of the meta tag.
+ * @param $tag_info
+ *   An array describing the meta tag.
+ */
+function hook_nodewords_tags_permission_alter(&$permission, $object, $tag_name, $tag_info) {
+  global $user;
+  if (user_access('administer meta tags')) {
+    $permission = TRUE;
+    return;
+  }
+
+  if ($object['type'] == 'node' && ($node = node_load($options['id']))) {
+    if ($user->uid == $node->uid && user_access("edit one's own node meta tags")) {
+      $permission = TRUE;
+      return;
+    }
+
+    if (user_access('edit any node meta tags')) {
+      $permission = TRUE;
+    }
+    else {
+      $permission = FALSE;
+    }
+  }
+  elseif ($object['type'] == 'user' && ($account = user_load($object['id']))) {
+    if ($user->uid == $account->uid && user_access("edit one's own user profile meta tags")) {
+      $permission = TRUE;
+      return;
+    }
+
+    if (user_access('edit any user profile meta tags')) {
+      $permission = TRUE;
+    }
+    else {
+      $permission = FALSE;
+    }
+  }
+}
+
+/**
+ * Declares the meta tags implemented by the module.
  *
  *
  * @return
  *   An array containing the following values:
  *
- *  - tag:context:allowed, tag:context:denied - these indexes define in which
- *    contexts the meta tags are allowed (and denied).
- *
- *  - tag:db:type - if the value of this index is not equal to string, the value
- *    of the meta tag is passed to serialize before to be saved in the database
- *    table used by Nodewords.
- *
- *  - tag:function:prefix - the prefix used when Nodewords looks for some
- *    functions it uses; the actual implementation uses the following functions
- *    (<suffix> stays for the value of this array index):
- *
- *    - <suffix>_form(&$form, $content, $options) - the function is used to
- *      populate the form used to edit the meta tags.
- *
- *    - <suffix>_prepare(&$tags, $content, $options) - the function is used to
- *      populate the array of meta tags before they are output in the page
- *      template.
- *
- *    - <suffix>_settings_form(&$form, $form_id, $options) - the function is
- *      used to populate a settings form; actually, the function is used to
- *      populate the Nodewords settings page ($form_id == 'nodewords_settings_form'),
- *      or the content type settings page ($form_id == 'node_type_form').
- *
- *  - tag:function:parameters - an array passed to the functions used by
- *    Nodewords as $options['parameters'] ($options is the last parameter
- *    passed to the functions.
- *
- *  - tag:template - the string used as template when the meta tag is added to
- *    the HTML tag HEAD; the value can be a constant as defined by the module, or
- *    a string.
- *
- *  - tag:weight - the weight used to order the meta tags before to output them
- *    in HTML; the lighter meta tag will be output first.
- *
- *  - widget:label - the label used as title in the fieldset for the form field
+ *  - attributes - the tag attributes used when outputting the tag on HTML HEAD.
+ *  - callback - the string used to built the name of the functions called for
+ *    any meta tags operations.
+ *  - context - the contexts in which the meta tags are allowed (and denied).
+ *  - label - the label used as title in the fieldset for the form field
  *    shown in the form to edit the meta tags values.
- *
- *  - widget:permission - the permission associated with the form fields used to
- *    edit the meta tags values; this permission is used only when the meta tag
- *    edit form fields are shown in a form that is accessible not only from the
- *    administrators users (in example, in the node edit form, or in the user
- *    profile form).
+ *  - templates - the templates used when the meta tag is output.
+ *  - weight - the weight used to order the meta tags before to output them;
+ *    the lighter meta tag will be output first.
  *
  */
 function hook_nodewords_tags_info() {
   $tags = array(
     'dc.title' => array(
-      'tag:context:denied' => array(NODEWORDS_TYPE_DEFAULT),
-      'tag:db:type' => 'string',
-      'tag:function:prefix' => 'test_metatags_dc_title',
-      'tag:template' => array('dc.title' => NODEWORDS_META),
-      'widget:label' => t('Dublin Core title'),
-      'widget:permission' => 'edit meta tag Dublin Core TITLE',
+      'callback' => 'nodewords_extra_dc_title',
+      'context' => array(
+        'denied' => array(
+          NODEWORDS_TYPE_DEFAULT,
+        ),
+      ),
+      'label' => t('Dublin Core title'),
+      'templates' => array(
+        'head' => array(
+          'dc.title' => NODEWORDS_META,
+        ),
+      ),
     ),
     'location' => array(
-      'tag:context:allowed' => array(NODEWORDS_TYPE_NODE, NODEWORDS_TYPE_USER),
-      'tag:db:type' => 'array',
-      'tag:function:prefix' => 'test_metatags_location',
-      'tag:template' => array(
-      'geo.position' => NODEWORDS_META,
-      'icbm' => NODEWORDS_META,
-    ),
-    'widget:label' => t('Location'),
-      'widget:permission' => 'edit location meta tag',
+      'callback' => 'nodewords_extra_location',
+      'label' => t('Location'),
+      'templates' => array(
+        'head' => array(
+          'geo.position' => NODEWORDS_META,
+          'icbm' => NODEWORDS_META,
+        )
+      ),
     ),
   );
 
@@ -103,9 +153,23 @@ function hook_nodewords_tags_info() {
 }
 
 /**
- * The hook is used to alter the metatags content.
+ * Alters the meta tags description passed by other modules.
  *
- * @param &$output
+ * @param &$tags_info
+ *   The array containing the information about the implemented meta tags.
+ *
+ * @see hook_nodewords_tags_info().
+ */
+function hook_nodewords_tags_info_alter(&$tags_info) {
+  if (isset($tags_info['abstract'])) {
+    $tags_info['abstract']['label'] = t('New label for abstract');
+  }
+}
+
+/**
+ * Alters the meta tags content.
+ *
+ * @param &$tags
  *  The array of meta tags values.
  * @param $parameters
  *  An array of parameters. The currently defined are:
@@ -115,14 +179,15 @@ function hook_nodewords_tags_info() {
  *   * output - where the meta tags are being output; the parameter value can
  *     'head' or 'update index'.
  */
-function hook_nodewords_tags_alter(&$output, $parameters) {
+function hook_nodewords_tags_alter(&$tags, $parameters) {
   if (empty($output['abstract']) && $parameters['type'] == NODEWORDS_TYPE_PAGE) {
     $output['abstract'] = t('Node content');
   }
 }
 
 /**
- * The hook is used to alter the string containing the metatags output.
+ * Alters the meta tags output.
+ * The hook is called when the meta tags are already rendered.
  *
  * @param &$output
  *  The string to alter.
@@ -130,7 +195,7 @@ function hook_nodewords_tags_alter(&$output, $parameters) {
  *  An array of parameters. The currently defined are:
  *   * type - the type of object for the page to which the meta
  *     tags are associated.
- *   * ids - the array of IDs for the object associated with the page.
+ *   * id - the ID for the object associated with the page.
  *   * output - where the meta tags are being output; the parameter value can
  *     'head' or 'update index'.
  */
@@ -140,7 +205,6 @@ function hook_nodewords_tags_output_alter(&$output, $parameters) {
     isset($parameters['output']) &&
     $parameters['output'] == 'head'
   );
-
   if ($bool) {
     $output = (
       '<link rel="schema.dc" href="http://purl.org/dc/elements/1.1/" />' . "\n" .
@@ -150,26 +214,19 @@ function hook_nodewords_tags_output_alter(&$output, $parameters) {
 }
 
 /**
- * The hook is used from the module to determinate the type of the object
- * associated with the currently viewed page (node, user, taxonomy term), and
- * the ID of the object.
+ * Determinates the type of the object associated with the viewed page.
  *
+ * @param &$result
+ *   the array used to write the result.
  * @param $arg
  *   the array as obtained from arg().
- *
- * @return
- *   An array containing the type of the object, and an array of IDs.
  */
-function hook_nodewords_type_id($arg) {
-  if ($arg[0] == 'tracker') {
-    return array(
-      NODEWORDS_TYPE_TRACKER, array(
-        isset($arg[1]) && is_numeric($arg[1]) ? array($arg[1]) : array(-1)
-      )
-    );
+function hook_nodewords_type_id(&$result, $arg) {
+  if ($arg[0] == 'user') {
+    // User page paths: user/$uid.
+    if (isset($arg[1]) && is_numeric($arg[1])) {
+      $result['type'] = NODEWORDS_TYPE_USER;
+      $result['id'] = $arg[1];
+    }
   }
 }
-
-/**
- * @} End of "addtogroup Nodewords hooks" .
- */
